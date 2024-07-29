0263dde4cf60bb0a0181cf19b54768ecf96601d2
mateusflores <mateusgf.42@gmais.com>
2024-07-28 23:33:47 -0300

    first commit

---
+++ LibManager/src/main/java/com/mateusflores/LibManager/LibManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mateusflores.LibManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibManagerApplication.class, args);
+	}
+
+}
---
+++ LibManager/src/test/java/com/mateusflores/LibManager/LibManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusflores.LibManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
